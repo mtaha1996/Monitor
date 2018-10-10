d8211e7fca436e206608005d7e8a84621cf79374
taha <mt.mehrab@gmail.com>
2018-10-10 16:23:00 +0330

    login done!

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
---
+++ env_monitor/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/taha/PycharmProjects/monitor/env_monitor/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
